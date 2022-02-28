3a80ee8c509e09f3649c9b4b9a78f08820914e47
andreachicco <andreachicco2002.ac@gmail.com>
2022-02-28 23:51:58 +0100

    connessione database

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,38 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/api/:anno', (req, res) => {
+//Connessione DB
+const dbCredentials = {
+  username: process.env.DBUSERNAME,
+  password: process.env.DBPASSWORD,
+  dbName: process.env.DBNAME
+}
+
+const mongoAtlasUri = `mongodb+srv://${dbCredentials.username}:${dbCredentials.password}@cluster0.urdgj.mongodb.net/${dbCredentials.dbName}?retryWrites=true&w=majority`;
+mongoose.connect(mongoAtlasUri, (err) => {
+  if(!err) console.log('Connessione DB riuscita')
+});
+
+const startingEndpoint = '/api/';
+
+app.get(`${startingEndpoint}:anno/:mese/:giorno/`, (req, res) => {
+  const { anno, mese, giorno } = req.params;
+
+  res.send(`Anno selezionato: ${anno}, mese: ${mese}, giorno: ${giorno}`);
+})
+
+/*app.get(`${startingEndpoint}:anno/:mese`, (req, res) => {
+  const { anno, mese } = req.params;
+  res.send(`Anno selezionato: ${anno}, mese: ${mese}`);
+})
+
+app.get(`${startingEndpoint}:anno`, (req, res) => {
   const { anno } = req.params;
   res.send(`Anno selezionato: ${anno}`);
-})
+})*/
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
