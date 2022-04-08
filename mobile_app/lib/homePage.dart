ece2e4baff17123f6d9dbfd1712bb5ee61ed29cb
Nicolò Catania <101711650+NicoloAkaCat@users.noreply.github.com>
2022-04-08 12:21:41 +0200

    Added Year selector

--- mobile_app/lib/homePage.dart
+++ mobile_app/lib/homePage.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key, required this.token }) : super(key: key);
@@ -9,6 +11,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final Uri urlYears = Uri.parse("http://conta-ore-straordinari.herokuapp.com/api/v1/years");
+  List<dynamic> years = [];
+  
+  AlertDialog errorAlert = AlertDialog(
+    title: const Text("Impossibile connettersi al Server",),
+    content: const Text("C'è stato un errore mentre nel collegamento con il sever, è consigliato controllare la proprio connessione internet e riprovare"),
+    actions: [
+      TextButton(onPressed: () => {}, 
+        child: const Text("Riprova")
+      )
+    ],
+  );
+
+  bool connected = false;
+  
+  Future getYears(token) async{
+    http.get(urlYears, 
+      headers: {
+        'authorization': token
+      }
+    ).then((value){
+      if(value.statusCode == 200){
+        years = jsonDecode(value.body);
+        setState(() {
+          connected = true;
+        });
+      }
+      else{
+        showDialog(context: context, 
+          builder: (BuildContext context) => errorAlert
+        );
+      }
+    });
+  }
+
+  @override
+  void initState(){
+    getYears(widget.token);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +64,40 @@ class _HomePageState extends State<HomePage> {
         automaticallyImplyLeading: false,
         backgroundColor: Theme.of(context).primaryColor
       ),
-      body: Text("body"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(height: 15),
+          connected ? Center(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5)
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: DropdownButton(
+                  icon: const Icon(Icons.arrow_drop_down_circle_outlined),
+                  iconSize: 18,
+                  borderRadius: BorderRadius.circular(10),
+                  value: years[0]['year'],
+                  items: years.map((dynamic e) {
+                    return DropdownMenuItem<dynamic>(
+                      value: e['year'],
+                      child: Text("${e['year']}",
+                        style: Theme.of(context).textTheme.headline2,
+                      ),
+                    );
+                  }).toList(), 
+                  onChanged: (dynamic year) {
+
+                  },
+                ),
+              ),
+            ),
+          ) : Text("")
+        ],
+      ),
     );
   }
 }
